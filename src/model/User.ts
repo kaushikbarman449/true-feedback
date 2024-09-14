9e0d655e32d0163db1736fa13a1b3814505682ae
kaushikbarman449 <kaushikbarman449@gmail.com>
2024-09-14 14:53:27 +0530

    project set-up. Initial commit

---
+++ src/model/User.ts
@@ -0,0 +1,12 @@
+import mongoose, {Schema, Document} from "mongoose";
+
+export interface Message extends Document{
+  content: string;
+  createdAt: Date
+}
+
+const MessageSchema: Schema<Message> = new Schema({
+  content:{
+    type: String
+  }
+})
